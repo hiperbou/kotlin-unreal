15a458cfcaec87ebf4e63af26ae6cc471347acc3
hiperbou <hiperbou@gmail.com>
2021-03-19 13:02:39 +0100

    Updated Unreal definitions

--- KotlinUnreal/src/ue/ueExtensions.kt
+++ KotlinUnreal/src/ue/ueExtensions.kt
@@ -1,8 +1,13 @@
 package ue
 
+import org.w3c.dom.events.Event
+
 /**
  * Created by hiperbou on 27/01/2017.
  */
 external class UClass{}
 
 
+external interface EventListenerObject {
+    fun handleEvent(evt: Event)
+}
